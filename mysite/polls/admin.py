7294ceb5dc6f9de047a1440f24b23f93026c52b4
prianthon <prianthonsubardio@gmail.com>
2016-12-17 13:52:51 +0700

    admin search

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -12,6 +12,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information',    {'fields':['pub_date'], 'classes': ['collapse']}),
     ]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
